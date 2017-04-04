e7b163f80a5d689f5d7ca84bbe006e9b828a36ad
Jan <janek.ziniewicz@gmail.com>
2017-04-04 16:24:13 +0200

    Create Vote and Proposal contracts.

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -1,13 +1,62 @@
 pragma solidity ^0.4.4;
 
+// TODO owned
+contract Vote {
+    event LogVote(address indexed addr);
+
+    string public name;
+
+    function Vote(string _name) {
+        name = _name;
+    }
+
+    function() {
+        LogVote(msg.sender);
+    }
+
+}
+
+// TODO owned
+// TODO send message on selfdestruct
+// TODO destroy Vote contracts as well
+contract Proposal {
+
+    // TODO move to struct
+    string title;
+    string url;
+    string hash;
+    uint ballotEnd;
+
+    address voteYes;
+    address voteNo;
+
+    uint constant startingBlock = block.number;
+
+    function Proposal(
+        string _title,
+        string _url,
+        string _hash,
+        uint _ballotEnd,
+        address _voteYes,
+        address _voteNo) public {
+        title = _title;
+        url = _url;
+        hash = _hash;
+        ballotEnd = _ballotEnd;
+        voteYes = _voteYes;
+        voteNo = _voteNo;
+    }
+
+}
+
 contract Ballot {
+
+    event NewBallot(address proposal, address voteYes, address voteNo);
 
     uint256 public constant requiredDeposit = 1 ether;
 
     function beginBallot(
         string title,
-        string author,
-        string description,
         string url,
         string hash,
         uint ballotEnd) external payable {
@@ -15,6 +64,18 @@ contract Ballot {
         if (msg.value != requiredDeposit || ballotEnd <= block.number) {
             throw;
         }
+
+        Vote voteYes = new Vote("yes");
+        Vote voteNo = new Vote("no");
+        Proposal proposal = new Proposal(
+            title,
+            url,
+            hash,
+            ballotEnd,
+            voteYes,
+            voteNo);
+
+        NewBallot(proposal, voteYes, voteNo);
 
     }
 
