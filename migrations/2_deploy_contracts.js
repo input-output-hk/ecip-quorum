f33c8ff9a983262281b194ab9f2a82fb48365638
Jan <janek.ziniewicz@gmail.com>
2017-04-10 18:13:00 +0200

    Cleaning up

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,5 @@
-// var ConvertLib = artifacts.require("./ConvertLib.sol");
-// var MetaCoin = artifacts.require("./MetaCoin.sol");
 var Ballot = artifacts.require("./Ballot.sol");
 
 module.exports = function(deployer) {
-    // deployer.deploy(ConvertLib);
-    // deployer.link(ConvertLib, MetaCoin);
-    // deployer.deploy(MetaCoin);
     deployer.deploy(Ballot);
 };
