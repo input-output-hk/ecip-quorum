353df9fe0b36a3fdd4e0975b214ec7ace41e1e81
Jan <janek.ziniewicz@gmail.com>
2017-04-11 17:59:54 +0200

    Save ballot data in BallotList contract

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -14,6 +14,14 @@ contract Ownable {
         _;
     }
 
+    function transferOwnership(address newOwner) onlyOwner {
+        if (newOwner != address(0)) {
+            owner = newOwner;
+        } else {
+            throw;
+        }
+    }
+
 }
 
 contract Vote is Ownable {
@@ -43,7 +51,6 @@ contract Vote is Ownable {
 
 contract Proposal is Ownable {
 
-    // TODO move to struct
     string public title;
     string public url;
     string public hash;
@@ -54,8 +61,7 @@ contract Proposal is Ownable {
 
     uint constant startingBlock = block.number;
 
-    function Proposal(
-                      string _title,
+    function Proposal(string _title,
                       string _url,
                       string _hash,
                       uint _ballotEnd,
@@ -75,6 +81,67 @@ contract Proposal is Ownable {
 
 }
 
+contract BallotList is Ownable {
+
+    uint public maxBallots;
+    string[] public ballots;
+
+    function BallotList(uint _maxBallots) {
+        if (_maxBallots <= 0) {
+            throw;
+        }
+        maxBallots = _maxBallots;
+    }
+
+    function addBallot(string title, string url, string hash, uint ballotEnd) external onlyOwner {
+        ballots.push(concatStrings(title, url, hash, ballotEnd));
+    }
+
+    function uintToBytes(uint v) constant internal returns (bytes32 ret) {
+        if (v == 0) {
+            ret = '0';
+        }
+        else {
+            while (v > 0) {
+                ret = bytes32(uint(ret) / (2 ** 8));
+                ret |= bytes32(((v % 10) + 48) * 2 ** (8 * 31));
+                v /= 10;
+            }
+        }
+        return ret;
+    }
+
+    // ['title','url','hash',234]
+    function concatStrings(string title, string url, string hash, uint ballotEnd) internal returns(string) {
+        bytes memory titleArr = bytes(title);
+        bytes memory urlArr = bytes(title);
+        bytes memory hashArr = bytes(title);
+        bytes32 ballotEndArr = uintToBytes(ballotEnd);
+        string memory data = new string(11 + titleArr.length + urlArr.length + hashArr.length + ballotEndArr.length);
+        bytes memory dataArr = bytes(data);
+        uint k = 0;
+        uint i = 0;
+        dataArr[k++] = '[';
+        dataArr[k++] = '\'';
+        for (i = 0; i < titleArr.length; i++) dataArr[k++] = titleArr[i];
+        dataArr[k++] = '\'';
+        dataArr[k++] = ',';
+        dataArr[k++] = '\'';
+        for (i = 0; i < urlArr.length; i++) dataArr[k++] = urlArr[i];
+        dataArr[k++] = '\'';
+        dataArr[k++] = ',';
+        dataArr[k++] = '\'';
+        for (i = 0; i < hashArr.length; i++) dataArr[k++] = hashArr[i];
+        dataArr[k++] = '\'';
+        dataArr[k++] = ',';
+        for (i = 0; i < ballotEndArr.length; i++) dataArr[k++] = ballotEndArr[i];
+        dataArr[k++] = ']';
+        return string(dataArr);
+    }
+
+
+}
+
 contract Ballot {
 
     event NewBallot(address proposal, address voteYes, address voteNo);
@@ -83,16 +150,26 @@ contract Ballot {
     uint public requiredDeposit;
     // in bytes
     uint public maxDataSize;
+    address public ballotList;
 
     // TODO allow for more than one ballot created by one account
     // TODO check if not modifiable from the outside?
     mapping(address => address) public ballots;
 
     // TODO option to register ballot on the list
-    function Ballot(uint _requiredDeposit, uint _maxDataSize) {
+    function Ballot(uint _requiredDeposit, uint _maxDataSize, uint maxBallots) {
         if (_requiredDeposit <= 0 || _maxDataSize <= 0) {
             throw;
         }
+
+        if (maxBallots > 0) {
+            BallotList _ballotList = new BallotList(maxBallots);
+            _ballotList.transferOwnership(this); // TODO works?
+            ballotList = _ballotList;
+        } else {
+            ballotList = 0;
+        }
+
         requiredDeposit = _requiredDeposit;
         maxDataSize = _maxDataSize;
     }
@@ -119,6 +196,10 @@ contract Ballot {
                                          voteNo);
 
         ballots[msg.sender] = proposal;
+
+        if (ballotList != 0) {
+            BallotList(ballotList).addBallot(title, url, hash, ballotEnd);
+        }
 
         NewBallot(proposal, voteYes, voteNo);
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,5 +2,5 @@ var Ballot = artifacts.require("./Ballot.sol");
 
 // TODO set separate deployment for production and testrpc
 module.exports = function(deployer) {
-    deployer.deploy(Ballot, web3.toWei(1, "ether"), 100);
+    deployer.deploy(Ballot, web3.toWei(1, "ether"), 100, 20);
 };
