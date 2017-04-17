845baf216b40aa2e8a0b12aea65683a538c8d954
Jan <janek.ziniewicz@gmail.com>
2017-04-17 17:26:04 +0200

    Add frontend

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,37 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './app/javascripts/app.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'app.js'
-  },
-  plugins: [
-    // Copy our app's index.html to the build folder.
-    new CopyWebpackPlugin([
-      { from: './app/index.html', to: "index.html" }
-    ])
-  ],
-  module: {
-    rules: [
-      {
-       test: /\.css$/,
-       use: [ 'style-loader', 'css-loader' ]
-      }
+    entry: './app/javascripts/app.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.js'
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: './app/index.html', to: "index.html" },
+            { from: './app/img/etc-logo.png', to: "img/etc-logo.png" }
+        ])
     ],
-    loaders: [
-      { test: /\.json$/, use: 'json-loader' },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015'],
-          plugins: ['transform-runtime']
-        }
-      }
-    ]
-  }
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            }
+        ],
+        loaders: [
+            {
+                test: /\.json$/,
+                use: 'json-loader' },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015'],
+                    plugins: ['transform-runtime']
+                }
+            }
+        ]
+    }
 }
